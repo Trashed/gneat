8de7948d71fc14b29d5753f482e774ab08181d01
Jose Kortelahti <jkortelahti@gmail.com>
2025-05-02 23:15:26 +0300

    Implement 'add new node' mutation
    +semver: minor

--- mutation/gene.go
+++ mutation/gene.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Trashed/gneat/genetics"
 )
 
+// ApplyRandWeight mutates the weight of a gene by adding a random value.
 func ApplyRandWeight(g *genetics.Gene, weightMutationRate float64, weightPerturbationStrength float64) {
 	randVal := rand.Float64()
 	negativeMultiplier := func() float64 {
@@ -16,4 +17,32 @@ func ApplyRandWeight(g *genetics.Gene, weightMutationRate float64, weightPerturb
 	}()
 
 	g.Weight += negativeMultiplier * (randVal * weightPerturbationStrength)
+}
+
+// AddNewNode inserts a new node by splitting an existing gene.
+// It creates a new gene and a new node, and returns them.
+func AddNewNode(g *genetics.Gene, innovation *genetics.InnovationStore) ([]*genetics.Gene, *genetics.Node) {
+
+	g.Enabled = false
+
+	newNode := genetics.NewNode(genetics.NodeHidden)
+	newGene1 := &genetics.Gene{
+		InNode:    g.InNode,
+		OutNode:   newNode,
+		Weight:    g.Weight,
+		Enabled:   true,
+		Recurrent: false,
+	}
+	innovation.AddGene(newGene1)
+
+	newGene2 := &genetics.Gene{
+		InNode:    newNode,
+		OutNode:   g.OutNode,
+		Weight:    1.0,
+		Enabled:   true,
+		Recurrent: false,
+	}
+	innovation.AddGene(newGene2)
+
+	return []*genetics.Gene{newGene1, newGene2}, newNode
 }
---
+++ mutation/gene_test.go
@@ -0,0 +1,47 @@
+package mutation_test
+
+import (
+	"testing"
+
+	"github.com/Trashed/gneat/genetics"
+	"github.com/Trashed/gneat/mutation"
+)
+
+func TestAddNewNode(t *testing.T) {
+	t.Parallel()
+
+	// Create a new gene
+	gene := &genetics.Gene{
+		InNode:    genetics.NewNode(genetics.NodeInput),
+		OutNode:   genetics.NewNode(genetics.NodeOutput),
+		Weight:    0.5,
+		Enabled:   true,
+		Recurrent: false,
+	}
+
+	const newNodeId = 3
+
+	store := genetics.NewInnovationStore()
+	if _, isAdded := store.AddGene(gene); !isAdded {
+		t.Error("Expected to add gene to innovation store, but it was not added")
+	}
+
+	// Add a new node to the gene
+	newGenes, newNode := mutation.AddNewNode(gene, store)
+
+	if newGenes == nil {
+		t.Error("Expected a new gene, got nil")
+	}
+
+	if newNode == nil {
+		t.Error("Expected a new node, got nil")
+	}
+
+	if newGenes[0].OutNode.Id != newNodeId {
+		t.Errorf("Expected new gene's OutNode ID to be %d, got %d", newNodeId, newGenes[0].OutNode.Id)
+	}
+
+	if newGenes[1].InNode.Id != newNodeId {
+		t.Errorf("Expected new gene's InNode ID to be %d, got %d", newNodeId, newGenes[1].InNode.Id)
+	}
+}
